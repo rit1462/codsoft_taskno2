6ab91b93e5412ae7b3cd60735de2a3f807ddc38d
rit1462 <117028678+rit1462@users.noreply.github.com>
2023-08-23 23:21:14 +0530

    Add files via upload

---
+++ codsoft_taskno2/main.js
@@ -0,0 +1,22 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+// function circleMouseFollower() { 
+//     window.addEventListener("mousemove" , function (dets) {
+//         document.querySelector(
+//             "#minicircle"
+//         ).style.transform = `translate(${dets.clientX}px , ${dets.clientY}px))`;
+//     });   
+//  }
+
+function circleMouseFollower() {
+    window.addEventListener("mousemove", function (dets) {
+      document.querySelector(
+        "#minicircle"
+      ).style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
+    });
+  }
+
+circleMouseFollower();
